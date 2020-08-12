5407f0cac0421f380a76612087610613e90a79d8
Luiz Koike <luiz.koike@bistwobis.com.br>
2020-08-12 09:16:36 -0300

    :bug: Fix typo

--- layouts/default.vue
+++ layouts/default.vue
@@ -130,7 +130,7 @@ export default {
           icon: 'mdi-apps',
           title: 'Welcome',
           to: '/'
-        },
+        }
         // {
         //   icon: 'mdi-chart-bubble',
         //   title: 'Inspire',
